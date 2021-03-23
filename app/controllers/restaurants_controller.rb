6a3aeab3fb6786417231b15368f7f456608c0025
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-03-23 11:08:13 -0600

    docs: Return the firs image to display in preview

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,25 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.first
-    render :json => @restaurants.images.map{|img| ({ image: url_for(img) })}
+    #@restaurants = Restaurant.all
+    #render json: {restaurant: @restaurant, images:  @restaurant.images.map{|img| ({ image: url_for(img) })}}
+    #render json: @restaurants.images.map{|img| ({ image: url_for(img) })}
+    @restaurants = []
+    Restaurant.all.map do |restaurant|
+      @restaurants.push({
+        id: restaurant.id,
+        title: restaurant.name,
+        description: restaurant.description,
+        cover: url_for(restaurant.images.first),
+      })
+    end
+    render json: @restaurants
   end
 
-  def create
-    @restaurant = Restaurant.create!(message_params)
-    render_success(message: 'OK')
+  def show
+    @restaurant = Restaurant.first
+    render json: {restaurant: @restaurant, images:  @restaurant.images.map{|img| ({ image: url_for(img) })}}
   end
+
 
   def create
     @restaurant = Restaurant.create!(restaurant_params)
